7eb973fe8aa73e549d232585ab64d2ea0c0ba602
Dan Chicu <38363167+MudaDayo@users.noreply.github.com>
2025-04-25 21:53:36 +0200

    menus are sort of working, no character select yet

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -7,4 +7,9 @@ public class LoadScene : MonoBehaviour
     {
         SceneManager.LoadScene(sceneName);
     }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 }
