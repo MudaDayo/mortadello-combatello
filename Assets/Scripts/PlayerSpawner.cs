e5ff0a99cdc8d1476905a52e875d26cb287365eb
Dan Chicu <38363167+MudaDayo@users.noreply.github.com>
2025-04-26 19:31:23 +0200

    player spawner works

--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+
+    public GameObject characterPrefab1;
+    public GameObject characterPrefab2;
     public GameObject player1Character;
     public GameObject player2Character;
       private PlayerManager playerManager;
@@ -14,6 +17,27 @@ public class PlayerSpawner : MonoBehaviour
         if (playerManager == null)
         {
             Debug.LogError("PlayerManager not found in the scene!");
+        }
+
+        if(playerManager.player1Character == 0)
+        {
+            player1Character = Instantiate(characterPrefab1, new Vector3(-3f, 0, 6), Quaternion.identity);
+        }
+        else if(playerManager.player1Character == 1)
+        {
+            player1Character = Instantiate(characterPrefab2, new Vector3(-3f, 0, 6), Quaternion.identity);
+        }
+
+        if(playerManager.player2Character == 0)
+        {
+            player2Character = Instantiate(characterPrefab1, new Vector3(3f, 0, 6), Quaternion.identity);
+            player2Character.transform.localScale = new Vector3(-1, 1, 1); // Flip the character
+
+        }
+        else if(playerManager.player2Character == 1)
+        {
+            player2Character = Instantiate(characterPrefab2, new Vector3(3f, 0, 6), Quaternion.identity);
+            player2Character.transform.localScale = new Vector3(-1, 1, 1); // Flip the character
         }
     }
 
