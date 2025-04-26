2fbf32607597dad4b9163fdfb5a83a95e60a433a
Dan Chicu <38363167+MudaDayo@users.noreply.github.com>
2025-04-26 19:24:37 +0200

    UI stuff

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,18 @@ public class UIManager : MonoBehaviour
     public Slider player1HealthBar;
     public Slider player2HealthBar;
 
+    public TextMeshProUGUI player1WinsText;
+    public TextMeshProUGUI player2WinsText;
+
+    void UpdateWinsText()
+    {
+        if (playerManager != null)
+        {
+            player1WinsText.text = playerManager.player1Wins > 0 ? "WINS: " + playerManager.player1Wins.ToString() : string.Empty;
+            player2WinsText.text = playerManager.player2Wins > 0 ? "WINS: " + playerManager.player2Wins.ToString() : string.Empty;
+        }
+    }
+
     // Round win counters
     public Image[] player1RoundWins; // Array of images for Player 1
     public Image[] player2RoundWins; // Array of images for Player 2
@@ -31,17 +43,22 @@ public class UIManager : MonoBehaviour
         if (playerManager == null)
         {
             Debug.LogError("PlayerManager not found in the scene!");
+        }else{
+            // Set character names based on playerManager values
+            player1Name.text = playerManager.player1Character == 0 ? "CAPPUCINO ASASSINO" : "LA VACA SATURNO SATURNITA";
+            player2Name.text = playerManager.player2Character == 0 ? "CAPPUCINO ASASSINO" : "LA VACA SATURNO SATURNITA";
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateWinsText();
         // Update health sliders based on PlayerManager health values
         if (playerManager != null)
         {
-            player1HealthBar.value = playerManager.player1Health;
-            player2HealthBar.value = playerManager.player2Health;
+            player1HealthBar.value = playerManager.player1Health / 100f;
+            player2HealthBar.value = playerManager.player2Health /100f;
 
             // Update round win images for Player 1
             for (int i = 0; i < player1RoundWins.Length; i++)
