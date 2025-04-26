0c096d3e5b440f6fca716b38c7704f4c08cb3250
Dan Chicu <38363167+MudaDayo@users.noreply.github.com>
2025-04-26 17:37:13 +0200

    okay

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,6 +3,17 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance { get; private set; }
+    [SerializeField] private GameObject playerPrefab1;
+    [SerializeField] private GameObject playerPrefab2;
+
+    [SerializeField] private float player1Health = 100f;
+    [SerializeField] private float player2Health = 100f;
+
+    [SerializeField] private float player1Rounds = 0f;
+    [SerializeField] private float player2Rounds = 0f;
+
+    [SerializeField] private float player1Wins = 0f;
+    [SerializeField] private float player2Wins = 0f;
 
     private void Awake()
     {
